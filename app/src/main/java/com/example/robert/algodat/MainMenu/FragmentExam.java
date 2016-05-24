1504f12f128426d3898f87b6f45295bb1181b396
Nasrin Daria <a1403475@unet.univie.ac.at>
2016-05-24 21:30:43 +0200

    FragmentExam class and layout have been changed.

--- app/src/main/java/com/example/robert/algodat/MainMenu/FragmentExam.java
+++ app/src/main/java/com/example/robert/algodat/MainMenu/FragmentExam.java
@@ -5,14 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.robert.algodat.R;
+
+import org.w3c.dom.Text;
 
 public class FragmentExam extends Fragment {
     /**
      * The fragment argument representing the section number for this
      * fragment.
      */
+    private ImageView examModeIcon;
+    private Switch examModeSwitch;
+    private TextView modeTextView;
+
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     public FragmentExam() {
@@ -34,6 +44,25 @@ public class FragmentExam extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_exam, container, false);
+
+        examModeIcon = (ImageView) rootView.findViewById(R.id.examModeIcon);
+        examModeIcon.setImageResource(R.mipmap.ic_hash);
+
+        modeTextView = (TextView) rootView.findViewById(R.id.modeTextView);
+
+        examModeSwitch = (Switch) rootView.findViewById(R.id.examModeSwitch);
+        examModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    examModeIcon.setImageResource(R.mipmap.ic_hard);
+                    modeTextView.setText("HARD");
+                } else {
+                    examModeIcon.setImageResource(R.mipmap.ic_easy);
+                    modeTextView.setText("EASY");
+                }
+            }
+        });
 
         return rootView;
     }
