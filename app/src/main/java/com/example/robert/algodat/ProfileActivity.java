d0f9db192a89257a45cd3f0a48afcd068d839ed8
Christian Lascsak <christian.lascsak@gmail.com>
2016-05-27 15:58:48 +0200

    Profile Activity updated

--- app/src/main/java/com/example/robert/algodat/ProfileActivity.java
+++ app/src/main/java/com/example/robert/algodat/ProfileActivity.java
@@ -3,12 +3,19 @@ package com.example.robert.algodat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.robert.algodat.R;
+
 public class ProfileActivity extends AppCompatActivity {
 
+    private ContentLoadingProgressBar progressbarExams;
+    private ContentLoadingProgressBar progressbarPractice;
+
+    //Gakgqlk Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +23,10 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        progressbarExams=(ContentLoadingProgressBar) findViewById(R.id.progressExams);
+        progressbarPractice=(ContentLoadingProgressBar) findViewById(R.id.progressPractice);
+        progressbarExams.show();
+        progressbarPractice.show();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
