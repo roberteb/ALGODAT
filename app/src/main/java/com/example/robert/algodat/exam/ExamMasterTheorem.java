3eaa0ecfd914642edc493c39d3764c17fd49c262
Nasrin Daria <a1403475@unet.univie.ac.at>
2016-05-31 13:55:51 +0200

    The color has been set to blue again!

--- app/src/main/java/com/example/robert/algodat/exam/ExamMasterTheorem.java
+++ app/src/main/java/com/example/robert/algodat/exam/ExamMasterTheorem.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -27,6 +27,9 @@ public class ExamMasterTheorem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_master_theorem);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setTitle("Exam");
 
         firstArrow = (ImageView) findViewById(R.id.firstExamArrow);
         firstArrow.setImageResource(R.drawable.arrow_seen);
@@ -71,5 +74,23 @@ public class ExamMasterTheorem extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        //Up button
+        super.onBackPressed();
+
+        return true;
     }
 }
--- app/src/main/java/com/example/robert/algodat/exam/ExamSelectionSort.java
+++ app/src/main/java/com/example/robert/algodat/exam/ExamSelectionSort.java
@@ -5,6 +5,7 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -27,6 +28,9 @@ public class ExamSelectionSort extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_selection_sort);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setTitle("Exam");
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -77,5 +81,22 @@ public class ExamSelectionSort extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        //Up button
+        super.onBackPressed();
+
+        return true;
     }
 }
