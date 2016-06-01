7baa33b0ab02f7fd56637a42d9d83c126fe41d22
roberteb <roberteb@live.at>
2016-06-01 10:30:33 +0200

    added title_screen

---
+++ app/src/main/java/com/example/robert/algodat/main_menu/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.robert.algodat.main_menu;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
