7ee808ad3da499434638feb036977813c534ea93
Nasrin Daria <a1403475@unet.univie.ac.at>
2016-06-10 15:47:11 +0200

    Exam icons and their functionality for first page

--- app/src/main/java/com/example/robert/algodat/exam/ExamActivity.java
+++ app/src/main/java/com/example/robert/algodat/exam/ExamActivity.java
@@ -1,6 +1,8 @@
 package com.example.robert.algodat.exam;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
@@ -10,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 import com.example.robert.algodat.R;
@@ -22,6 +26,12 @@ public class ExamActivity extends AppCompatActivity {
     FragmentTransaction ft;
     Button examNextButton;
     ViewSwitcher examViewSwitcher;
+    ImageView firstArrowImageView;
+    ImageView secondArrowImageView;
+    ImageView thirdArrowImageView;
+    ImageView fourthArrowImageView;
+    ImageView fifthArrowImageView;
+    TextView timerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,12 @@ public class ExamActivity extends AppCompatActivity {
 
         examNextButton = (Button) findViewById(R.id.examNextButton);
         examViewSwitcher = (ViewSwitcher) findViewById(R.id.examViewSwitcher);
+        firstArrowImageView = (ImageView) findViewById(R.id.firstArrowImageView);
+        secondArrowImageView = (ImageView) findViewById(R.id.secondArrowImageView);
+        thirdArrowImageView = (ImageView) findViewById(R.id.thirdArrowImageView);
+        fourthArrowImageView = (ImageView) findViewById(R.id.fourthArrowImageView);
+        fifthArrowImageView = (ImageView) findViewById(R.id.fifthArrowImageView);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,10 +61,26 @@ public class ExamActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 examViewSwitcher.showNext();
+                secondArrowImageView.setImageResource(R.drawable.arrow_seen);
             }
         });
 //        hash
 
+        new CountDownTimer(5400000, 1000) {
+            private long seconds = 60;
+
+            public void onTick(long millisUntilFinished) {
+                if(seconds==0){
+                    seconds = 60;
+                }
+                timerTextView.setText(millisUntilFinished / 1000/60/60+":"+millisUntilFinished / 1000/60%60+":"+seconds--);
+//                remainedSeconds = millisUntilFinished;
+            }
+
+            public void onFinish() {
+                timerTextView.setText("done!");
+            }
+        }.start();
 
 
     }
